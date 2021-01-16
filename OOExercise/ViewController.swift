f2dd62381e9dbd8bf618e1ee89d7db64dd2b59bd
Paul Lee <ctwdtw@gmail.com>
2021-01-16 11:25:38 +0800

    first commit

---
+++ OOExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OOExercise
+//
+//  Created by Paul Lee on 2021/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
