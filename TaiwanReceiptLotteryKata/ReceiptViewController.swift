e8c07931e15a5c55035159785740081dd8565cd5
ctwdtw <ctwdtw@gmail.com>
2021-09-13 15:31:05 +0800

    delete experiment solution

--- TaiwanReceiptLotteryKata/ReceiptViewController.swift
+++ TaiwanReceiptLotteryKata/ReceiptViewController.swift
@@ -8,50 +8,6 @@
 import UIKit
 
 public class ReceiptViewController: UIViewController {
-
-    private var receiptViewModel: ReceiptPresentable!
     
-    public convenience init(viewModel: ReceiptPresentable) {
-        self.init()
-        self.receiptViewModel = viewModel
-    }
-    
-    public lazy var receiptTypeLabel: UILabel = {
-        let label = UILabel()
-        //
-        // .. layout code goes here
-        //
-        return label
-    }()
-    
-    public lazy var lotteryNumberLabel: UILabel = {
-        let label = UILabel()
-        //
-        // .. layout code goes here
-        //
-        return label
-    }()
-    
-    public lazy var receiptStatusLabel: UILabel = {
-        let label = UILabel()
-        //
-        // .. layout code goes here
-        //
-        return label
-    }()
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        _ = receiptTypeLabel
-        _ = lotteryNumberLabel
-        _ = receiptStatusLabel
-        displayReceipt()
-    }
-    
-    private func displayReceipt() {
-        receiptTypeLabel.text = receiptViewModel.presentReceiptType()
-        lotteryNumberLabel.text = receiptViewModel.presentReceiptLotteryNumber()
-        receiptStatusLabel.text = receiptViewModel.presentReceiptStatus()
-    }
 }
 
