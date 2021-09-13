11cf5969c2d475d79b047b0e99d1aafe52960bf6
ctwdtw <ctwdtw@gmail.com>
2021-09-13 17:59:14 +0800

    add platform independent target `TaiwanReceiptLottery`

---
+++ TaiwanReceiptLotteryTests/TaiwanReceiptLotteryTests.swift
@@ -0,0 +1,16 @@
+//
+//  TaiwanReceiptLotteryTests.swift
+//  TaiwanReceiptLotteryTests
+//
+//  Created by Paul Lee on 2021/9/13.
+//
+
+import XCTest
+import TaiwanReceiptLottery
+
+class TaiwanReceiptLotteryTests: XCTestCase {
+    func test_can_pass() {
+        let a = 1
+        XCTAssertEqual(a, 1)
+    }
+}
