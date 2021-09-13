ed821f51898ec8e3c4830da84fb60ea8da2c1d0f
ctwdtw <ctwdtw@gmail.com>
2021-09-13 18:57:48 +0800

    present b2c receipt with mobile bar code

--- TaiwanReceiptLotteryTests/TaiwanReceiptLotteryTests.swift
+++ TaiwanReceiptLotteryTests/TaiwanReceiptLotteryTests.swift
@@ -43,6 +43,41 @@ class Receipt {
     }
 }
 
+class B2CReceipt {
+    private var date: Date
+    private var price: Int
+    private(set) var lotteryNumber: String
+    private(set) var mobileBarCode: String
+    
+    init(date: Date, price: Int, lotteryNumber: String, mobileBarCode: String) {
+        self.date = date
+        self.price = price
+        self.lotteryNumber = lotteryNumber
+        self.mobileBarCode = mobileBarCode
+    }
+}
+
+class B2CReceiptViewModel {
+    private var receipt: B2CReceipt
+    
+    init(_ receipt: B2CReceipt) {
+        self.receipt = receipt
+    }
+    
+    var title: String {
+        return "A B2C receipt has been issued."
+    }
+    
+    var body: String {
+        return "The lottery number is \(receipt.lotteryNumber)."
+    }
+    
+    var footer: String {
+        return "The receipt is saved in cloud database with mobile barcode number: \(receipt.mobileBarCode)"
+    }
+    
+}
+
 class TaiwanReceiptLotteryTests: XCTestCase {
     func test_presentB2BReceipt() {
         let b2bReceipt = Receipt(date: Date(), price: 100, lotteryNumber: "AA-00000001", taxID: "45002931")
@@ -51,5 +86,14 @@ class TaiwanReceiptLotteryTests: XCTestCase {
         XCTAssertEqual(vm.title, "A B2B receipt has been issued, the company tax id is 45002931.", "title message")
         XCTAssertEqual(vm.body, "The lottery number is AA-00000001.", "body message")
         XCTAssertEqual(vm.footer, "You can choose to print out this receipt or send it to your customer through email.", "footer message")
+    }
+    
+    func test_presentB2CReceiptWithMobileBarCode() {
+        let b2bReceipt = B2CReceipt(date: Date(), price: 100, lotteryNumber: "AA-00000001", mobileBarCode: "/AB201C9")
+        let vm = B2CReceiptViewModel(b2bReceipt)
+
+        XCTAssertEqual(vm.title, "A B2C receipt has been issued.", "title message")
+        XCTAssertEqual(vm.body, "The lottery number is AA-00000001.", "body message")
+        XCTAssertEqual(vm.footer, "The receipt is saved in cloud database with mobile barcode number: /AB201C9", "footer message")
     }
 }
