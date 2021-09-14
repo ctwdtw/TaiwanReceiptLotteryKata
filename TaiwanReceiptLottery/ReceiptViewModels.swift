b713adf4eb06f5035bca830a937077915c3f8a27
ctwdtw <ctwdtw@gmail.com>
2021-09-14 17:31:03 +0800

    add default implementation to `ReceiptViewModel` protocol to remove duplicate code among concreated receipt view models

--- TaiwanReceiptLottery/ReceiptViewModels.swift
+++ TaiwanReceiptLottery/ReceiptViewModels.swift
@@ -8,75 +8,13 @@
 import Foundation
 
 public protocol ReceiptViewModel {
+    var receipt: Receipt { get }
     var title: String { get }
     var body: String { get }
     var footer: String { get }
 }
 
-public struct B2BReceiptViewModel: ReceiptViewModel {
-    public var title: String {
-        "A B2B receipt has been issued, the company tax id is \(receipt.taxID)."
-    }
-    
-    public var body: String {
-        "The lottery number is \(receipt.lotteryNumber)."
-    }
-    
-    public var footer: String {
-        "You can choose to print out this receipt or send it to your customer through email."
-    }
-    
-    private let receipt: B2BReceipt
-    
-    public init(_ receipt: B2BReceipt) {
-        self.receipt = receipt
-    }
-    
-}
-
-public struct MobileBarCodeReceiptViewModel: ReceiptViewModel {
-    public var title: String {
-        "A B2C receipt has been issued."
-    }
-    
-    public var body: String {
-        "The lottery number is \(receipt.lotteryNumber)."
-    }
-    
-    public var footer: String {
-        "The receipt is saved in cloud database with mobile barcode number: \(receipt.mobileBarCode)"
-    }
-    
-    private let receipt: MobileBarCodeReceipt
-    
-    public init(_ receipt: MobileBarCodeReceipt) {
-        self.receipt = receipt
-    }
-    
-}
-
-public struct NPOReceiptViewModel: ReceiptViewModel {
-    public var title: String {
-        "A B2C receipt has been issued."
-    }
-    
-    public var body: String {
-        "The lottery number is \(receipt.lotteryNumber)."
-    }
-    
-    public var footer: String {
-        "The lottery opportunity has been donated to a non profit organization, the organization id is: \(receipt.npoCode)"
-    }
-    
-    private let receipt: NPOCodeReceipt
-    
-    public init(_ receipt: NPOCodeReceipt) {
-        self.receipt = receipt
-    }
-    
-}
-
-public struct PrintedB2CReceiptViewModel: ReceiptViewModel {
+extension ReceiptViewModel {
     public var title: String {
         "A B2C receipt has been issued."
     }
@@ -88,10 +26,71 @@ public struct PrintedB2CReceiptViewModel: ReceiptViewModel {
     public var footer: String {
         "The receipt has been printed."
     }
+}
+
+public struct B2BReceiptViewModel: ReceiptViewModel {
+    public var receipt: Receipt {
+        return _receipt
+    }
     
-    private let receipt: PrintedB2CReceipt
+    public var title: String {
+        "A B2B receipt has been issued, the company tax id is \(_receipt.taxID)."
+    }
+    
+    public var footer: String {
+        "You can choose to print out this receipt or send it to your customer through email."
+    }
+    
+    private let _receipt: B2BReceipt
+    
+    public init(_ receipt: B2BReceipt) {
+        self._receipt = receipt
+    }
+    
+}
+
+public struct MobileBarCodeReceiptViewModel: ReceiptViewModel {
+    public var receipt: Receipt {
+        _receipt
+    }
+    
+    public var footer: String {
+        "The receipt is saved in cloud database with mobile barcode number: \(_receipt.mobileBarCode)"
+    }
+    
+    private let _receipt: MobileBarCodeReceipt
+    
+    public init(_ receipt: MobileBarCodeReceipt) {
+        self._receipt = receipt
+    }
+    
+}
+
+public struct NPOReceiptViewModel: ReceiptViewModel {
+    public var receipt: Receipt {
+        _receipt
+    }
+    
+    public var footer: String {
+        "The lottery opportunity has been donated to a non profit organization, the organization id is: \(_receipt.npoCode)"
+    }
+    
+    private let _receipt: NPOCodeReceipt
+    
+    public init(_ receipt: NPOCodeReceipt) {
+        self._receipt = receipt
+    }
+    
+}
+
+public struct PrintedB2CReceiptViewModel: ReceiptViewModel {
+    public var receipt: Receipt {
+        _receipt
+    }
+    
+    private let _receipt: PrintedB2CReceipt
     
     public init(_ receipt: PrintedB2CReceipt) {
-        self.receipt = receipt
+        self._receipt = receipt
     }
 }
