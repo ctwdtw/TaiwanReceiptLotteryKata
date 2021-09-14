13703a2702c5efb69313dea4fdf7e0e34b48fa7a
ctwdtw <ctwdtw@gmail.com>
2021-09-14 13:16:38 +0800

    add target `TaiwanReceiptLottery` and its test target

---
+++ TaiwanReceiptLotteryTests/TaiwanReceiptLotteryTests.swift
@@ -0,0 +1,18 @@
+//
+//  TaiwanReceiptLotteryTests.swift
+//  TaiwanReceiptLotteryTests
+//
+//  Created by Paul Lee on 2021/9/14.
+//
+
+import XCTest
+import `TaiwanReceiptLottery`
+
+class TaiwanReceiptLotteryTests: XCTestCase {
+
+    func test_can_run() {
+        let a = 1
+        XCTAssertEqual(a, 1)
+    }
+
+}
